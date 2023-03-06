02c4ec8eb932dd10b764430ac404e087eb897b34
José Gallegos <jose.gallegos.delcarpio@gmail.com>
2023-03-06 12:13:11 -0600

    v-select icons test done!

--- src/views/VoiceMessage.vue
+++ src/views/VoiceMessage.vue
@@ -1,58 +1,31 @@
 <template>
-  <v-combobox v-model="selectedCode" v-model:search="phoneNumberSearch" :items="countryCodes" item-title="searchText"
-    item-value="code" return-object label="Select an item.." hide-no-data hide-details>
-  </v-combobox>
+    <v-container>
+        <v-row>
+            <v-col cols="12" md="5">
+                <v-card>
+                    <v-select v-model="select" :items="items" item-value="abbr" label="País" return-object
+                        :item-title="(item) => `${item.country}, ${item.code}`">
+                        <template v-slot:selection="{ item }">
+                            <v-icon v-if="item.value">{{ item.value.icon }}</v-icon>
+                        </template>
+                    </v-select>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
-
+  
 <script>
 export default {
-  data: () => ({
-    phoneNumberSearch: null,
-    selectedCode: null,
-    countryCodes: [],
-    countries: [
-      {
-        code: '93',
-        country: 'Afghanistan',
-        icon: 'mdi-flag'
-      },
-      {
-        code: '94',
-        country: 'Test2',
-        icon: 'mdi-flag'
-      },
-      {
-        code: '95',
-        country: 'Prueba',
-        icon: 'mdi-flag'
-      },
-    ],
-  }),
-
-  watch: {
-    phoneNumberSearch(val) {
-      val && val !== this.phoneNumberSelected && this.test(val)
-    },
-  },
-  methods: {
-    test(v) {
-      const searchLowerCase = v.toLowerCase();
-      this.countryCodes = this.countries.filter((item) => {
-        const countryLowerCase = item.country.toLowerCase();
-        const codeLowerCase = item.code.toLowerCase();
-        const searchText = `${countryLowerCase} (${codeLowerCase})`.toLowerCase();
-
-        return searchText.includes(searchLowerCase);
-      }).map((item) => {
-        const country = item.country;
-        const code = item.code;
-
+    data() {
         return {
-          ...item,
-          searchText: `${country} (${code})`
-        };
-      });
-    }
-  }
+            select: { country: 'Afghanistan', code: '+93', icon: 'mdi-flag' },
+            items: [
+                { country: 'Afghanistan', code: '+93', icon: 'mdi-flag' },
+                { country: 'Albania', code: '+355', icon: 'mdi-flag-triangle' },
+            ],
+        }
+    },
 }
 </script>
+  

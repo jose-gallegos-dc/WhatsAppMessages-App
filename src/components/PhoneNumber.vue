c9d527c98ab8c9527d6968ac3228da1a18b09bf5
jose-gallegos-dc <jose.gallegos.delcarpio@gmail.com>
2023-03-10 00:06:18 -0600

    tabulations 3

--- src/components/PhoneNumber.vue
+++ src/components/PhoneNumber.vue
@@ -1,96 +1,96 @@
 <template>
-    <v-row>
-        <v-col cols="4" class="pr-1">
-            <v-select color="teal-darken-3" v-model="countryCodeSelected" :items="countryCodeItems" item-value="code"
-                label="País" return-object :item-title="(item) => `${item.country}  (${item.code})`">
-                <template v-slot:selection="{ item }">
-                    <v-icon v-if="item.value">{{ item.value.icon }}</v-icon>
-                </template>
-            </v-select>
-        </v-col>
-        <v-col cols="8" class="pl-1">
-            <v-autocomplete color="teal-darken-3"
-                label="Número de teléfono" v-on:keypress="NumbersOnly" v-model.lazy="phoneNumberSelected"
-                v-model:search="phoneNumberSearch" :items="phoneNumberItems" :loading="phoneNumberLoading" hide-no-data
-                clearable @change="onPhoneNumberChange" :hint="isOriginalPhoneNumber" persistent-hint/>
-        </v-col>
-    </v-row>
+	<v-row>
+		<v-col cols="4" class="pr-1">
+			<v-select color="teal-darken-3" v-model="countryCodeSelected" :items="countryCodeItems" item-value="code"
+				label="País" return-object :item-title="(item) => `${item.country}  (${item.code})`">
+				<template v-slot:selection="{ item }">
+					<v-icon v-if="item.value">{{ item.value.icon }}</v-icon>
+				</template>
+			</v-select>
+		</v-col>
+		<v-col cols="8" class="pl-1">
+			<v-autocomplete color="teal-darken-3" label="Número de teléfono" v-on:keypress="NumbersOnly"
+				v-model.lazy="phoneNumberSelected" v-model:search="phoneNumberSearch" :items="phoneNumberItems"
+				:loading="phoneNumberLoading" hide-no-data clearable @change="onPhoneNumberChange"
+				:hint="isOriginalPhoneNumber" persistent-hint />
+		</v-col>
+	</v-row>
 </template>
   
 <script>
 export default {
-    name: "PhoneNumberComponent",
-    data: () => ({
-        isOriginalPhoneNumber: "",
-        countryCodeSelected: { country: "Afghanistan", code: "+93", icon: "fi fi-af" },
-        countryCodeItems: [
-            { country: "Afghanistan", code: "+93", icon: "fi fi-af" },
-            { country: "Albania", code: "+355", icon: "fi fi-al" },
-        ],
-        phoneNumberSelected: "",
-        phoneNumberSearch: null,
-        phoneNumberItems: [],
-        phoneNumberLoading: false,
-        phoneNumbers: [
-            "9612491813",
-            "9611234567",
-            "9611234568",
-            "9611234569",
-            "9611234560",
-            "9611234565",
-        ],
-        originalPhoneNumbers: [],
-    }),
-    watch: {
-        phoneNumberSearch(val) {
-            val && val !== this.phoneNumberSelected && this.phoneNumberSelections(val);
-        },
-    },
-    created() {
-        this.originalPhoneNumbers = this.phoneNumbers.slice();
-    },
-    methods: {
-        NumbersOnly(evt) {
-            evt = (evt) ? evt : window.event;
-            let charCode = (evt.which) ? evt.which : evt.keyCode;
-            if ((charCode > 31 && (charCode < 48 || charCode > 57)) && charCode !== 46) {
-                evt.preventDefault();
-            } else {
-                return true;
-            }
-        },
-        phoneNumberSelections(v) {
-            this.phoneNumberLoading = true;
+	name: "PhoneNumberComponent",
+	data: () => ({
+		isOriginalPhoneNumber: "",
+		countryCodeSelected: { country: "Afghanistan", code: "+93", icon: "fi fi-af" },
+		countryCodeItems: [
+			{ country: "Afghanistan", code: "+93", icon: "fi fi-af" },
+			{ country: "Albania", code: "+355", icon: "fi fi-al" },
+		],
+		phoneNumberSelected: "",
+		phoneNumberSearch: null,
+		phoneNumberItems: [],
+		phoneNumberLoading: false,
+		phoneNumbers: [
+			"9612491813",
+			"9611234567",
+			"9611234568",
+			"9611234569",
+			"9611234560",
+			"9611234565",
+		],
+		originalPhoneNumbers: [],
+	}),
+	watch: {
+		phoneNumberSearch(val) {
+			val && val !== this.phoneNumberSelected && this.phoneNumberSelections(val);
+		},
+	},
+	created() {
+		this.originalPhoneNumbers = this.phoneNumbers.slice();
+	},
+	methods: {
+		NumbersOnly(evt) {
+			evt = (evt) ? evt : window.event;
+			let charCode = (evt.which) ? evt.which : evt.keyCode;
+			if ((charCode > 31 && (charCode < 48 || charCode > 57)) && charCode !== 46) {
+				evt.preventDefault();
+			} else {
+				return true;
+			}
+		},
+		phoneNumberSelections(v) {
+			this.phoneNumberLoading = true;
 
-            // Simulated ajax query
-            setTimeout(() => {
-                // Check if the value is already in the list
-                const valueExists = this.phoneNumbers.some((phoneNumber) => {
-                    return phoneNumber === v;
-                });
+			// Simulated ajax query
+			setTimeout(() => {
+				// Check if the value is already in the list
+				const valueExists = this.phoneNumbers.some((phoneNumber) => {
+					return phoneNumber === v;
+				});
 
-                if (!valueExists && v !== "") {
-                    // Add the new value to the list
-                    this.phoneNumbers.push(v);
-                }
+				if (!valueExists && v !== "") {
+					// Add the new value to the list
+					this.phoneNumbers.push(v);
+				}
 
-                // Filter the list of phone numbers
-                this.phoneNumberItems = this.phoneNumbers.filter((e) => {
-                    return (e || "").indexOf(v || "") > -1;
-                });
+				// Filter the list of phone numbers
+				this.phoneNumberItems = this.phoneNumbers.filter((e) => {
+					return (e || "").indexOf(v || "") > -1;
+				});
 
-                this.phoneNumberLoading = false;
-            }, 500);
-        },
-        onPhoneNumberChange() {
-            if (this.originalPhoneNumbers.includes(this.phoneNumberSelected)) {
-                this.isOriginalPhoneNumber = "";
-                console.log("Número seleccionado válido");
-            } else {
-                this.isOriginalPhoneNumber = "Teléfono no registrado.";
-                console.log("Número seleccionado inválido");
-            }
-        },
-    },
+				this.phoneNumberLoading = false;
+			}, 500);
+		},
+		onPhoneNumberChange() {
+			if (this.originalPhoneNumbers.includes(this.phoneNumberSelected)) {
+				this.isOriginalPhoneNumber = "";
+				console.log("Número seleccionado válido");
+			} else {
+				this.isOriginalPhoneNumber = "Teléfono no registrado.";
+				console.log("Número seleccionado inválido");
+			}
+		},
+	},
 };
 </script>
--- src/views/TextMessage.vue
+++ src/views/TextMessage.vue
@@ -1,62 +1,61 @@
 <template>
-    <v-container>
-        <v-row>
-            <v-col cols="12" md="5">
-                <v-card elevation="0" class="pa-4 border rounded-lg">
-                    <PhoneNumber ref="PhoneNumberComponent" />
-                </v-card>
-            </v-col>
-            <v-spacer></v-spacer>
-            <v-col cols="12" md="7">
-                <v-card elevation="0" class="pa-4 border rounded-lg">
-                    <v-form @submit.prevent="onSubmit" v-model="valid">
-                        <v-textarea color="teal-darken-3" label="Mensaje" rows="5" row-height="15" v-model="message"
-                            :rules="messageRules" :counter="255"></v-textarea>
-                        <div class="d-flex justify-end">
-                            <v-btn type="submit" class="mt-3" color="teal-darken-2" prepend-icon="mdi-send"
-                                :disabled="!valid">
-                                Enviar
-                            </v-btn>
-                        </div>
-                    </v-form>
-                </v-card>
-            </v-col>
-        </v-row>
-    </v-container>
+	<v-container>
+		<v-row>
+			<v-col cols="12" md="5">
+				<v-card elevation="0" class="pa-4 border rounded-lg">
+					<PhoneNumber ref="PhoneNumberComponent" />
+				</v-card>
+			</v-col>
+			<v-spacer></v-spacer>
+			<v-col cols="12" md="7">
+				<v-card elevation="0" class="pa-4 border rounded-lg">
+					<v-form @submit.prevent="onSubmit" v-model="valid">
+						<v-textarea color="teal-darken-3" label="Mensaje" rows="5" row-height="15" v-model="message"
+							:rules="messageRules" :counter="255"></v-textarea>
+						<div class="d-flex justify-end">
+							<v-btn type="submit" class="mt-3" color="teal-darken-2" prepend-icon="mdi-send" :disabled="!valid">
+								Enviar
+							</v-btn>
+						</div>
+					</v-form>
+				</v-card>
+			</v-col>
+		</v-row>
+	</v-container>
 </template>
 
 <script>
 import PhoneNumber from '@/components/PhoneNumber.vue';
 
 export default {
-    name: 'TextMessage',
-    components: {
-        PhoneNumber
-    },
-    data: () => ({
-        message: '',
-        valid: false,
-        messageRules: [
-            value => {
-                if (value) return true;
-                return 'El mensaje es requerido.';
-            },
-            value => {
-                if (value?.length <= 255) return true;
-                return 'El mensaje debe tener menos de 255 caracteres.';
-            },
-            value => {
-                if (value && value.trim().length > 0) return true;
-                return 'El mensaje no puede tener solo espacios en blanco.';
-            }
-        ],
-    }),
-    methods: {
-        onSubmit() {
-            let phoneNumber = this.$refs.PhoneNumberComponent.phoneNumberSelected;
-            let countryCode = this.$refs.PhoneNumberComponent.countryCodeSelected.code;
-            console.log(countryCode + phoneNumber + ' ' + this.message);
-        }
-    }
+	name: 'TextMessage',
+	components: {
+		PhoneNumber
+	},
+	data: () => ({
+		message: '',
+		valid: false,
+		messageRules: [
+			value => {
+				if (value) return true;
+				return 'El mensaje es requerido.';
+			},
+			value => {
+				if (value?.length <= 255) return true;
+				return 'El mensaje debe tener menos de 255 caracteres.';
+			},
+			value => {
+				if (value && value.trim().length > 0) return true;
+				return 'El mensaje no puede tener solo espacios en blanco.';
+			}
+		],
+	}),
+	methods: {
+		onSubmit() {
+			let phoneNumber = this.$refs.PhoneNumberComponent.phoneNumberSelected;
+			let countryCode = this.$refs.PhoneNumberComponent.countryCodeSelected.code;
+			console.log(countryCode + phoneNumber + ' ' + this.message);
+		}
+	}
 }
 </script>
